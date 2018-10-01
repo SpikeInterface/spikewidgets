fd38bb674b368a8b6bf13199d4eb42d06b5c4639
Jeremy Magland <jeremy.magland@gmail.com>
2018-10-01 12:02:01 -0400

    travis and pypi

--- devel/publish.sh
+++
@@ -1,3 +0,0 @@
-rm -rf build/ dist/ ml_ephys.egg-info/
-python3 setup.py sdist bdist_wheel
-twine upload dist/*
--- setup.py
+++ setup.py
@@ -2,9 +2,15 @@ import setuptools
 
 pkg_name="spikewidgets"
 
+import unittest
+def my_test_suite():
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover('tests', pattern='test_*.py')
+    return test_suite
+
 setuptools.setup(
     name=pkg_name,
-    version="0.1.0",
+    version="0.1.2",
     author="Jeremy Magland",
     author_email="jmagland@flatironinstitute.org",
     description="Python widgets for use with spike sorting and electrophysiology",
@@ -14,11 +20,13 @@ setuptools.setup(
         'numpy',
         'ipython',
         'vdom',
-        'ipywidgets'
+        'ipywidgets',
+        'spikeinterface'
     ],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
-    )
+    ),
+    test_suite='setup.my_test_suite'
 )
