79cfc9bfb3ba10472f84865a884d58c628ffd0f3
Jeremy Magland <jeremy.magland@gmail.com>
2018-10-04 22:23:33 -0400

    add

---
+++ spikewidgets/example_datasets/real.py
@@ -0,0 +1,10 @@
+import spikeinterface as si
+import numpy as np
+
+def real(name='franklab_tetrode',download=True):
+    if name == 'franklab_tetrode':
+        dsdir='kbucket://b5ecdf1474c5/datasets/neuron_paper/franklab_tetrode'
+        IX=si.MdaRecordingExtractor(dataset_directory=dsdir,download=download)
+        return (IX,None)
+    else:
+        raise Exception('Unrecognized name for real dataset: '+name)
