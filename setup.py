f661fe9a3605b1b09fb8c2c021187cb8f88293d4
Jeremy Magland <jeremy.magland@gmail.com>
2018-10-17 22:24:51 -0400

    change deploy password

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ pkg_name="spikewidgets"
 
 setuptools.setup(
     name=pkg_name,
-    version="0.1.13",
+    version="0.1.14",
     author="Jeremy Magland",
     author_email="jmagland@flatironinstitute.org",
     description="Python widgets for use with spike sorting and electrophysiology",
