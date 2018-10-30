f65df7574626cc213cceb7b82af1ff6ae3a2952a
Jeremy Magland <jeremy.magland@gmail.com>
2018-10-30 18:38:59 -0400

    add pandas dependency

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ pkg_name="spikewidgets"
 
 setuptools.setup(
     name=pkg_name,
-    version="0.1.17",
+    version="0.1.18",
     author="Jeremy Magland",
     author_email="jmagland@flatironinstitute.org",
     description="Python widgets for use with spike sorting and electrophysiology",
@@ -18,7 +18,8 @@ setuptools.setup(
         'ipywidgets',
         'matplotlib',
         'spikeinterface',
-        'ml_ms4alg'
+        'ml_ms4alg',
+        'pandas'
     ],
     classifiers=(
         "Programming Language :: Python :: 3",
