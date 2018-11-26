85edb3a2123b973a27db317d17d9f03c42cfc5f7
Jeremy Magland <jeremy.magland@gmail.com>
2018-11-26 11:39:24 -0500

    test deploy

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ pkg_name = "spikewidgets"
 
 setuptools.setup(
     name=pkg_name,
-    version="0.1.22",
+    version="0.1.23",
     author="Jeremy Magland",
     author_email="jmagland@flatironinstitute.org",
     description="Python widgets for use with spike sorting and electrophysiology",
