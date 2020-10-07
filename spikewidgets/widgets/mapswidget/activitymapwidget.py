3543510c2056ede375d682051342b09e287cc961
Matthias H Hennig <mhhennig@gmail.com>
2020-10-07 20:35:24 +0100

    The reviewer wanted it...

--- spikewidgets/widgets/mapswidget/activitymapwidget.py
+++ spikewidgets/widgets/mapswidget/activitymapwidget.py
@@ -1,12 +1,15 @@
 import numpy as np
 import spiketoolkit as st
 import matplotlib.pylab as plt
+import matplotlib as mpl
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 from ..utils import LabeledRectangle
 from spikewidgets.widgets.basewidget import BaseWidget
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 
 def plot_activity_map(recording, channel_ids=None, trange=None, cmap='viridis', background='on', label_color='r',
-                      transpose=False, frame=False, ax=None, figure=None):
+                      transpose=False, frame=False, ax=None, figure=None, colorbar=False):
     """
     Plots spike rate (estimated using simple threshold detector) as 2D activity map.
 
@@ -45,6 +48,7 @@ def plot_activity_map(recording, channel_ids=None, trange=None, cmap='viridis', 
         frame=frame,
         figure=figure,
         ax=ax,
+        colorbar=colorbar
     )
     W.plot()
     return W
@@ -53,7 +57,7 @@ def plot_activity_map(recording, channel_ids=None, trange=None, cmap='viridis', 
 class ActivityMapWidget(BaseWidget):
 
     def __init__(self, recording, channel_ids, trange, cmap, background, label_color='r', transpose=False, frame=False,
-                 figure=None, ax=None):
+                 figure=None, ax=None, colorbar=False):
         BaseWidget.__init__(self, figure, ax)
         self._recording = recording
         self._channel_ids = channel_ids
@@ -63,6 +67,8 @@ class ActivityMapWidget(BaseWidget):
         self._frame = frame
         self._bg = background
         self._label_color = label_color
+        self._show_colorbar = colorbar
+        self.colorbar = None
         self.name = 'ActivityMap'
         assert 'location' in self._recording.get_shared_channel_property_names(), "Activity map requires 'location'" \
                                                                                   "property"
@@ -80,7 +86,9 @@ class ActivityMapWidget(BaseWidget):
         locations = self._recording.get_channel_locations(channel_ids=self._channel_ids)
         activity = st.postprocessing.compute_channel_spiking_activity(self._recording,
                                                                       start_frame=self._trange[0],
-                                                                      end_frame=self._trange[1])
+                                                                      end_frame=self._trange[1],
+                                                                      normalize=False, 
+                                                                      recompute_info=False)
         if self._transpose:
             locations = np.roll(locations, 1, axis=1)
 
@@ -110,6 +118,9 @@ class ActivityMapWidget(BaseWidget):
         self._drs = []
         elec_x = 0.9 * pitch_x
         elec_y = 0.9 * pitch_y
+        activity = activity/(self._trange[1]-self._trange[0]) # spikes/s
+        max_activity = np.ceil(np.max(activity)*100)/100 # normalise to 1 for colours
+        activity = activity/max_activity
         for (loc, act, ch) in zip(locations, activity, self._recording.get_channel_ids()):
             color = cm(act)
             rect = plt.Rectangle((loc[0] - elec_x / 2, loc[1] - elec_y / 2), elec_x, elec_y,
@@ -127,3 +138,10 @@ class ActivityMapWidget(BaseWidget):
             self.ax.add_patch(rect)
         self.ax.axis('equal')
         self.ax.axis('off')
+        if self._show_colorbar:
+            cax = inset_axes(self.ax, width="0.5%", height="50%", loc='upper left', bbox_to_anchor=(0.02, 0., 1, 1),bbox_transform=self.ax.transAxes)
+            self.colorbar = plt.gcf().colorbar(mpl.collections.PatchCollection(self.ax.patches), cax=cax, orientation='vertical', shrink=0.5)
+            cax.yaxis.set_ticks_position('left')
+            cax.yaxis.set_label_position('left')
+            self.colorbar.set_ticks((0,1))
+            self.colorbar.set_ticklabels((0,max_activity))
