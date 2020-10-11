689af02b52c5473ae27ade23ea7a14cb005ab3e2
Alessio Buccino <alejoe9187@gmail.com>
2020-10-11 11:55:07 +0200

    Improve colorbar handling

--- spikewidgets/widgets/mapswidget/activitymapwidget.py
+++ spikewidgets/widgets/mapswidget/activitymapwidget.py
@@ -9,7 +9,8 @@ from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 
 def plot_activity_map(recording, channel_ids=None, trange=None, cmap='viridis', background='on', label_color='r',
-                      transpose=False, frame=False, ax=None, figure=None, colorbar=False):
+                      transpose=False, frame=False, colorbar=False, colorbar_bbox=None,
+                      colorbar_orientation='vertical', colorbar_width=0.02, ax=None, figure=None):
     """
     Plots spike rate (estimated using simple threshold detector) as 2D activity map.
 
@@ -23,10 +24,20 @@ def plot_activity_map(recording, channel_ids=None, trange=None, cmap='viridis', 
         List with start time and end time
     cmap: matplotlib colormap
         The colormap to be used (default 'viridis')
+    background: bool
+        If True, a background is added in between electrodes
     transpose: bool, optional, default: False
         Swap x and y channel coordinates if True.
     frame: bool, optional, default: False
         Draw a frame around the array if True.
+    colorbar: bool
+        If True, a colorbar is displayed
+    colorbar_bbox: bbox
+        Bbox (x,y,w,h) in figure coordinates to plot colorbar
+    colorbar_orientation: str
+        'vertical' or 'horizontal'
+    colorbar_width: float
+        Width of colorbar in figure coordinates (default 0.02)
     figure: matplotlib figure
         The figure to be used. If not given a figure is created
     ax: matplotlib axis
@@ -48,16 +59,20 @@ def plot_activity_map(recording, channel_ids=None, trange=None, cmap='viridis', 
         frame=frame,
         figure=figure,
         ax=ax,
-        colorbar=colorbar
+        colorbar=colorbar,
+        colorbar_bbox=colorbar_bbox,
+        colorbar_orientation=colorbar_orientation,
+        colorbar_width=colorbar_width
     )
     W.plot()
+
     return W
 
 
 class ActivityMapWidget(BaseWidget):
-
     def __init__(self, recording, channel_ids, trange, cmap, background, label_color='r', transpose=False, frame=False,
-                 figure=None, ax=None, colorbar=False):
+                 colorbar=False, colorbar_bbox=None, colorbar_orientation='vertical', colorbar_width=0.02,
+                 figure=None, ax=None):
         BaseWidget.__init__(self, figure, ax)
         self._recording = recording
         self._channel_ids = channel_ids
@@ -68,6 +83,9 @@ class ActivityMapWidget(BaseWidget):
         self._bg = background
         self._label_color = label_color
         self._show_colorbar = colorbar
+        self._colorbar_bbox = colorbar_bbox
+        self._colorbar_orient = colorbar_orientation
+        self._colorbar_width = colorbar_width
         self.colorbar = None
         self.name = 'ActivityMap'
         assert 'location' in self._recording.get_shared_channel_property_names(), "Activity map requires 'location'" \
@@ -87,8 +105,11 @@ class ActivityMapWidget(BaseWidget):
         activity = st.postprocessing.compute_channel_spiking_activity(self._recording,
                                                                       start_frame=self._trange[0],
                                                                       end_frame=self._trange[1],
-                                                                      normalize=False, 
-                                                                      recompute_info=False)
+                                                                      method='detection',
+                                                                      align=False,
+                                                                      normalize=False,
+                                                                      recompute_info=False,
+                                                                      verbose=True)
         if self._transpose:
             locations = np.roll(locations, 1, axis=1)
 
@@ -118,9 +139,13 @@ class ActivityMapWidget(BaseWidget):
         self._drs = []
         elec_x = 0.9 * pitch_x
         elec_y = 0.9 * pitch_y
-        activity = activity/(self._trange[1]-self._trange[0]) # spikes/s
-        max_activity = np.ceil(np.max(activity)*100)/100 # normalise to 1 for colours
-        activity = activity/max_activity
+
+        self._max_activity = np.round(np.max(activity), 2)
+
+        # normalize
+        activity -= np.min(activity)
+        activity /= np.ptp(activity)
+
         for (loc, act, ch) in zip(locations, activity, self._recording.get_channel_ids()):
             color = cm(act)
             rect = plt.Rectangle((loc[0] - elec_x / 2, loc[1] - elec_y / 2), elec_x, elec_y,
@@ -130,18 +155,57 @@ class ActivityMapWidget(BaseWidget):
             dr.connect()
             self._drs.append(dr)
 
-        self.ax.set_xlim(np.min(x) - pitch_x, np.max(x) + pitch_x)
-        self.ax.set_ylim(np.min(y) - pitch_y, np.max(y) + pitch_y)
         if self._frame:
             rect = plt.Rectangle((np.min(x) - pitch_x, np.min(y) - pitch_y), np.max(x) - np.min(x) + 2 * pitch_x,
                                  np.max(y) - np.min(y) + 2 * pitch_y, fill=None, edgecolor='k')
             self.ax.add_patch(rect)
+
         self.ax.axis('equal')
         self.ax.axis('off')
+        self.ax.set_xlim(np.min(x) - pitch_x, np.max(x) + pitch_x)
+        self.ax.set_ylim(np.min(y) - pitch_y, np.max(y) + pitch_y)
+
         if self._show_colorbar:
-            cax = inset_axes(self.ax, width="0.5%", height="50%", loc='upper left', bbox_to_anchor=(0.02, 0., 1, 1),bbox_transform=self.ax.transAxes)
-            self.colorbar = plt.gcf().colorbar(mpl.collections.PatchCollection(self.ax.patches), cax=cax, orientation='vertical', shrink=0.5)
+            # The canvas need to be drawn to get the right transforms
+            self.figure.canvas.draw()
+
+            if self._colorbar_bbox is None:
+                if self._colorbar_orient == 'vertical':
+                    colorbar_width = self._colorbar_width
+                    bottom_left = (np.min(x) - pitch_x, np.min(y) - pitch_y)
+                    top_left = (np.min(x) - pitch_x, np.max(y) + pitch_y)
+                    figure_to_data = self.figure.transFigure + self.ax.transData.inverted()
+                    data_to_figure = figure_to_data.inverted()
+                    corners_figure = data_to_figure.transform([bottom_left, top_left])
+
+                    bottom_left_fig = corners_figure[0]
+                    colorbar_height = (corners_figure[1] - corners_figure[0])[1]
+                    bbox = (bottom_left_fig[0] - 1.5 * colorbar_width, bottom_left_fig[1],
+                            colorbar_width, colorbar_height)
+                else:
+                    colorbar_height = self._colorbar_width
+                    bottom_left = (np.min(x) - pitch_x, np.min(y) - pitch_y)
+                    bottom_right = (np.max(x) + pitch_x, np.min(y) - pitch_y)
+                    figure_to_data = self.figure.transFigure + self.ax.transData.inverted()
+                    data_to_figure = figure_to_data.inverted()
+                    corners_figure = data_to_figure.transform([bottom_left, bottom_right])
+
+                    bottom_left_fig = corners_figure[0]
+                    colorbar_width = (corners_figure[1] - corners_figure[0])[0]
+
+                    bbox = (bottom_left_fig[0], bottom_left_fig[1] - 1.5*colorbar_height,
+                            colorbar_width, colorbar_height)
+
+            else:
+                bbox = self._colorbar_bbox
+
+            cax = self.figure.add_axes(bbox)
+            scalable = mpl.cm.ScalarMappable(norm=mpl.colors.Normalize(vmin=0, vmax=1), cmap=self._cmap)
+            self.colorbar = self.figure.colorbar(scalable, cax=cax,
+                                                 orientation=self._colorbar_orient)#, shrink=0.5)
             cax.yaxis.set_ticks_position('left')
             cax.yaxis.set_label_position('left')
-            self.colorbar.set_ticks((0,1))
-            self.colorbar.set_ticklabels((0,max_activity))
+            self.colorbar.set_ticks((0, 1))
+            self.colorbar.set_ticklabels((0, self._max_activity))
+
+
